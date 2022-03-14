a3179e0baa2d5c3d1a6ea1b6790e48077d2dc6fb
TPavZ <tmpavlic@gmail.com>
2022-03-14 11:40:10 -0500

    added functrion to get a random value.

--- src/Main/App.java
+++ src/Main/App.java
@@ -1,10 +1,18 @@
 package Main;
+import java.util.ArrayList;
+import java.util.Random;
+
 
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
-
+	
+	public static String getRandomValue(String[] list){
+	Random rand = new Random();
+	int randomNumber = rand.nextInt(list.length);
+	String resultString = list[randomNumber];
+	return resultString;
+	}
 }
