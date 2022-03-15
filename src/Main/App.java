38d4b5c7227e5358f8fc180497b32695fe63ff0f
TPavZ <tmpavlic@gmail.com>
2022-03-15 13:36:26 -0500

    removed repeat code.

--- src/Main/App.java
+++ src/Main/App.java
@@ -5,10 +5,6 @@ import java.util.Scanner;
 
 
 public class App {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	}
 	
 	public static String getRandomValue(String[] list){
 		Random rand = new Random();
@@ -34,5 +30,29 @@ public class App {
 		String[] settings = {"Milwaukee", "Tampa", "Denver"};
 		String[] weapons = {"Gun", "Knife", "Hammer"};
 		String[] villains = {"Agent Smith", "Palpatine", "Voldemort"};
+		
+		String settingsString = getRandomValue(settings);
+		String weaponsString = getRandomValue(weapons);
+		String villainsString = getRandomValue(villains);
+		
+		ArrayList<String> myAdventure = new ArrayList<String>(); // Create an ArrayList object
+		myAdventure.add(settingsString);
+		myAdventure.add(weaponsString);
+		myAdventure.add(villainsString);
+		
+		System.out.println("Completed Adventue:");
 	}
+	
+	
 }
+
+// 		*Java Choose Your Own Adventure*
+//	As a developer, I want to start a new Java Project and create an App.java class that contains a main() method.
+//	As a developer, I want to research “array literal” in Java so I can easily create three arrays of Strings.
+//	As a hero, I want my adventure to consist of a randomly chosen setting, weapon, and villain, chosen from my three Array[String]s.
+//	As a hero, I want the selected values to be saved in an ArrayList<String> called myAdventure.
+//	As a hero, I want to loop over myAdventure to display all the original selected values.
+//	As a hero, I want to be able to randomly re-select each of the values one at a time if chosen
+//	As a hero, I want to replace the original value in the ArrayList with the new one I have selected in the same spot.
+//	As a hero, I want to be given the option to reselect continually until I decide I am satisfied.
+//	As a hero, I want to display my completed adventure in the terminal.
